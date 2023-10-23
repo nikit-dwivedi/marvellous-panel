e5ca7f75bf47a0ce0803d2b29ab7ba9088482b83
nikitdwivedi <nikitdwivedi1997@gmail.com>
2023-10-23 15:26:07 +0530

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   hmr: false,
-  apiUrl: 'https://api.marvellousinfo.com/v1',
+  apiUrl: 'https://rest.marvellousinfo.com/v1',
   pdfUrl:'https://rest.apitemplate.io/v2/create-pdf?template_id=ff677b238eb9c31a'
 };
